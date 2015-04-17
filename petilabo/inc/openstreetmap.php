f4d8fe445024eb6786df04d8b715232e35c480d5
PetiLabo <petilabo@gmail.com>
2015-04-18 00:00:06 +0200

    PetiLabo Analitix : specific compression for maps

--- petilabo/inc/openstreetmap.php
+++ petilabo/inc/openstreetmap.php
@@ -1,24 +1,14 @@
 <?php
-/**
- * staticMapLite 0.3.1
- * Copyright 2009 Gerhard Koch
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * @author Gerhard Koch <gerhard.koch AT ymail.com>
- *
- * Simplifié et retouché pour PetiLabo par Philippe GILLES
- */
 
+define("_OPENSTREETMAP_SRC_URL", "http://tile.openstreetmap.org/{Z}/{X}/{Y}.png");
+define("_OPENSTREETMAP_TAILLE_TUILE", "256");
+define("_OPENSTREETMAP_COMPRESSION_SITE", "90");
+define("_OPENSTREETMAP_COMPRESSION_ADMIN", "80");
+ 
 class openstreetmap {
-    protected $tileSize = 256;
-    protected $tileSrcUrl = array('mapnik' => 'http://tile.openstreetmap.org/{Z}/{X}/{Y}.png',
-        'osmarenderer' => 'http://otile1.mqcdn.com/tiles/1.0.0/osm/{Z}/{X}/{Y}.png',
-        'cycle' => 'http://a.tile.opencyclemap.org/cycle/{Z}/{X}/{Y}.png',
-    );
+    protected $tailleTuile = 1;
 
 	// Propriétés des marqueurs
-    protected $tileDefaultSrc = 'mapnik';
     protected $osmLogo = 'osm-logo.png';
 	protected $markerFilename = 'osm-marker.png';
 	protected $markerShadow = 'osm-marker-shadow.png';
@@ -40,10 +30,9 @@ class openstreetmap {
 	// Propriétés communes aux deux modes
 	protected $zoom = 0;
     protected $markerBaseDir = null;
-	protected $maptype = null;
 
 	public function __construct() {
-        $this->maptype = $this->tileDefaultSrc;
+		$this->tailleTuile = (int) _OPENSTREETMAP_TAILLE_TUILE;
 	}
     public function prepareMapSite($root, $lat, $lon, $width, $height, $zoom) {
         $this->lat = $lat;$this->lon = $lon;
@@ -71,80 +60,76 @@ class openstreetmap {
     private function initCoords() {
         $this->centerX = $this->lonToTile($this->lon, $this->zoom);
         $this->centerY = $this->latToTile($this->lat, $this->zoom);
-        $this->offsetX = floor((floor($this->centerX) - $this->centerX) * $this->tileSize);
-        $this->offsetY = floor((floor($this->centerY) - $this->centerY) * $this->tileSize);
+        $this->offsetX = floor((floor($this->centerX) - $this->centerX) * $this->tailleTuile);
+        $this->offsetY = floor((floor($this->centerY) - $this->centerY) * $this->tailleTuile);
     }
 
     private function createSiteMap() {
         $this->image = imagecreatetruecolor($this->width, $this->height);
-        $startX = floor($this->centerX - ($this->width / $this->tileSize) / 2);
-        $startY = floor($this->centerY - ($this->height / $this->tileSize) / 2);
-        $endX = ceil($this->centerX + ($this->width / $this->tileSize) / 2);
-        $endY = ceil($this->centerY + ($this->height / $this->tileSize) / 2);
-        $this->offsetX = -floor(($this->centerX - floor($this->centerX)) * $this->tileSize);
-        $this->offsetY = -floor(($this->centerY - floor($this->centerY)) * $this->tileSize);
+        $startX = floor($this->centerX - ($this->width / $this->tailleTuile) / 2);
+        $startY = floor($this->centerY - ($this->height / $this->tailleTuile) / 2);
+        $endX = ceil($this->centerX + ($this->width / $this->tailleTuile) / 2);
+        $endY = ceil($this->centerY + ($this->height / $this->tailleTuile) / 2);
+        $this->offsetX = -floor(($this->centerX - floor($this->centerX)) * $this->tailleTuile);
+        $this->offsetY = -floor(($this->centerY - floor($this->centerY)) * $this->tailleTuile);
         $this->offsetX += floor($this->width / 2);
         $this->offsetY += floor($this->height / 2);
-        $this->offsetX += floor($startX - floor($this->centerX)) * $this->tileSize;
-        $this->offsetY += floor($startY - floor($this->centerY)) * $this->tileSize;
+        $this->offsetX += floor($startX - floor($this->centerX)) * $this->tailleTuile;
+        $this->offsetY += floor($startY - floor($this->centerY)) * $this->tailleTuile;
 
         for ($x = $startX; $x <= $endX; $x++) {
             for ($y = $startY; $y <= $endY; $y++) {
-                $url = str_replace(array('{Z}', '{X}', '{Y}'), array($this->zoom, $x, $y), $this->tileSrcUrl[$this->maptype]);
+                $url = str_replace(array('{Z}', '{X}', '{Y}'), array($this->zoom, $x, $y), _OPENSTREETMAP_SRC_URL);
                 $tileData = $this->fetchTile($url);
                 if ($tileData) {
                     $tileImage = imagecreatefromstring($tileData);
                 } else {
-                    $tileImage = imagecreate($this->tileSize, $this->tileSize);
+                    $tileImage = imagecreate($this->tailleTuile, $this->tailleTuile);
                     $color = imagecolorallocate($tileImage, 255, 255, 255);
                     @imagestring($tileImage, 1, 127, 127, 'err', $color);
                 }
-                $destX = ($x - $startX) * $this->tileSize + $this->offsetX;
-                $destY = ($y - $startY) * $this->tileSize + $this->offsetY;
-                imagecopy($this->image, $tileImage, $destX, $destY, 0, 0, $this->tileSize, $this->tileSize);
+                $destX = ($x - $startX) * $this->tailleTuile + $this->offsetX;
+                $destY = ($y - $startY) * $this->tailleTuile + $this->offsetY;
+                imagecopy($this->image, $tileImage, $destX, $destY, 0, 0, $this->tailleTuile, $this->tailleTuile);
             }
         }
     }
 	
     private function createAdminMap() {
-		$width = ((int) (1 + $this->to_x - $this->from_x)) * $this->tileSize;
-		$height = ((int) (1 + $this->to_y - $this->from_y)) * $this->tileSize;
+		$width = ((int) (1 + $this->to_x - $this->from_x)) * $this->tailleTuile;
+		$height = ((int) (1 + $this->to_y - $this->from_y)) * $this->tailleTuile;
         $this->image = imagecreatetruecolor($width, $height);
         for ($x = $this->from_x; $x <= $this->to_x; $x++) {
             for ($y = $this->from_y; $y <= $this->to_y; $y++) {
-                $url = str_replace(array('{Z}', '{X}', '{Y}'), array($this->zoom, $x, $y), $this->tileSrcUrl[$this->maptype]);
+                $url = str_replace(array('{Z}', '{X}', '{Y}'), array($this->zoom, $x, $y), _OPENSTREETMAP_SRC_URL);
                 $tileData = $this->fetchTile($url);
                 if ($tileData) {
                     $tileImage = imagecreatefromstring($tileData);
                 } else {
-                    $tileImage = imagecreate($this->tileSize, $this->tileSize);
+                    $tileImage = imagecreate($this->tailleTuile, $this->tailleTuile);
                     $color = imagecolorallocate($tileImage, 255, 255, 255);
                     @imagestring($tileImage, 1, 127, 127, 'err', $color);
                 }
-                $destX = ((int) ($x - $this->from_x)) * $this->tileSize;
-                $destY = ((int) ($y - $this->from_y)) * $this->tileSize;
-                imagecopy($this->image, $tileImage, $destX, $destY, 0, 0, $this->tileSize, $this->tileSize);
+                $destX = ((int) ($x - $this->from_x)) * $this->tailleTuile;
+                $destY = ((int) ($y - $this->from_y)) * $this->tailleTuile;
+                imagecopy($this->image, $tileImage, $destX, $destY, 0, 0, $this->tailleTuile, $this->tailleTuile);
             }
         }
     }
 
     private function placeMarker() {	
-		// set some local variables
-		$markerLat = $this->lat;
-		$markerLon = $this->lon;
+		$markerLat = $this->lat;$markerLon = $this->lon;
 		$markerImg = imagecreatefrompng($this->markerBaseDir . '/' . $this->markerFilename);
 		$markerShadowImg = imagecreatefrompng($this->markerBaseDir . '/' . $this->markerShadow);
 
-		// calc position
-		$destX = floor(($this->width / 2) - $this->tileSize * ($this->centerX - $this->lonToTile($markerLon, $this->zoom)));
-		$destY = floor(($this->height / 2) - $this->tileSize * ($this->centerY - $this->latToTile($markerLat, $this->zoom)));
-
-		// copy shadow on basemap
+		// Calcul de la position
+		$destX = floor(($this->width / 2) - $this->tailleTuile * ($this->centerX - $this->lonToTile($markerLon, $this->zoom)));
+		$destY = floor(($this->height / 2) - $this->tailleTuile * ($this->centerY - $this->latToTile($markerLat, $this->zoom)));
+		// Ajout de l'ombre
 		if ($markerShadowImg) {
 			imagecopy($this->image, $markerShadowImg, $destX + intval($this->markerShadowOffsetX), $destY + intval($this->markerShadowOffsetY), 0, 0, imagesx($markerShadowImg), imagesy($markerShadowImg));
 		}
-
-		// copy marker on basemap above shadow
+		// Ajout du marqueur
 		imagecopy($this->image, $markerImg, $destX + intval($this->markerImageOffsetX), $destY + intval($this->markerImageOffsetY), 0, 0, imagesx($markerImg), imagesy($markerImg));
     }
 
@@ -169,14 +154,13 @@ class openstreetmap {
 			$this->createSiteMap();
 			$this->placeMarker();
 			$this->copyrightNotice();
+			if ($this->image) {@imagejpeg($this->image, $src, (int) _OPENSTREETMAP_COMPRESSION_SITE);}
 		}
 		elseif ($this->isMapAdmin) {
 			$this->createAdminMap();
 			$this->copyrightNotice();
+			if ($this->image) {@imagejpeg($this->image, $src, (int) _OPENSTREETMAP_COMPRESSION_ADMIN);}
 		}
-		if ($this->image) {
-			@imagejpeg($this->image, $src, 90);
-			@imagedestroy($this->image);
-		}
+		if ($this->image) {@imagedestroy($this->image);}
     }
 }
